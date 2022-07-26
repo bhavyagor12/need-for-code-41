d265c1120d6426bd940b58afdd2316325a5346dc
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-07-26 12:12:49 +0530

    added server

---
+++ server/models/users.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: "string",
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: "string",
+    },
+    img: {
+      type: "string",
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subcribedUsers: {
+      type: [String],
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { signup, signin, signout } from "../controllers/auth.js";
+
+const router = express.Router();
+
+//create a user
+
+router.post("/signup", signup);
+//sign in
+router.post("/signin", signin);
+router.get("/logout", signout);
