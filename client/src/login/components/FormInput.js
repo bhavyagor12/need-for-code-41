59ded9b5aeb53136de2a3675f34fbd1686fda5f4
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-07-26 23:53:51 +0530

    aa

--- client/src/login/components/FormInput.js
+++ client/src/login/components/FormInput.js
@@ -4,10 +4,6 @@ import './FormInput.css'
 const FormInput = (props) => {
     const{label,errorMessage,onChange,id,...others}= props;
     const[focused,setFocused]= useState(false);
-
-    const handleFocus=()=>{
-      setFocused(true);
-    }
   return (
     <div className='formInput'>
         <label>{label}</label>
